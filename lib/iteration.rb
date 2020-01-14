a3fa515d5740a376a927f044b5a6f07789feccb8
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-14 21:45:57 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -29,7 +29,7 @@ total = 0
 row_index = 0
 while row_index < src.count
     if src[row_index][0] % 2 == 0 and src[row_index][1] % 2 == 0 
-        total += src[raw_index][0] + src[raw_index][1]
+        total += src[row_index][0] + src[row_index][1]
   end
         row_index += 1
 end

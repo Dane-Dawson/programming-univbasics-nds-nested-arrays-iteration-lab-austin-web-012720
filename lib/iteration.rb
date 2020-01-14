3ca725f34207f27998ec5eb66874fa20ba45e79d
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-14 21:30:37 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,44 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  pizza_array = []
+  row_index = 0
+  while row_index < src.count do
+    pizza_array << "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+      row_index += 1
+  end
 end
+
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+outer_results = []
+row_index = 0
+while row_index < src.count
+  element_index = 0
+  inner_results = []
+  while element_index < src[row_index].count
+    if src[raw_index][0] < src[raw_index][1]
+      inner_results.push src[raw_index][0]
+    else 
+      inner_results.push src[raw_index][1]
+    end
+    element_index += 1
+  end
+  outer_results << inner_results
+  row_index += 1
+end
+outer_results
 end
 
+
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+total = 0
+row_index = 0
+while row_index < src.count
+  element_index = 0
+  while element_index < total_even_pairs[row_index].count 
+    if src[element_index] % 2 == 1 and src[element_index + 1] % 2 == 1 
+        total += src[raw_index][0] + src[raw_index][1]
+      row_index += 1
+  end
+end
+  total
 end
